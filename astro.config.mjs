50135cb8e3d3773fa4312c0cc77995c971f9042c
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-03-25 22:48:07 +1000

    Not using SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
+  // output: "server",
+  // adapter: netlify(),
 });
--- src/pages/api/v1/getRelays.ts
+++ src/pages/api/v1/getRelays.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
 import { getRelays } from "@utils/getRelays";
 
-export const POST: APIRoute = async () => {
+export const GET: APIRoute = async () => {
   // Get the data
   const response = await getRelays();
   return response;
