9bdb3750506135503e2c35653286bab2c6a9e44c
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-03-25 14:00:29 +1000

    Add new endpoint

--- src/pages/api/v1/getRelaysByUser.ts
+++ src/pages/api/v1/getRelaysByUser.ts
@@ -1,8 +1,11 @@
 import type { APIRoute } from "astro";
 import { getRelays } from "@utils/getRelays";
 
-export const GET: APIRoute = async (address: `0x${string}`) => {
+export const GET: APIRoute = async ({ request }) => {
+  const address = new URL(request.url).searchParams.get(
+    "address"
+  ) as `0x${string}`;
   // Get the data
-  const response = await getRelays();
+  const response = await getRelays(address);
   return response;
 };
