d003ff1b0fdf9b491508bfc69b63fae2b1140444
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-04-04 15:35:15 +1000

    Adding dev and prod config

--- src/utils/warp.config.ts
+++ src/utils/warp.config.ts
@@ -1,9 +1,13 @@
 import { WarpFactory } from "warp-contracts";
 import { EthersExtension } from "warp-contracts-plugin-ethers";
 
-const warp = WarpFactory.forMainnet({
+const warpDev = WarpFactory.forMainnet({
   inMemory: true,
   dbLocation: "./cache/warp",
 }).use(new EthersExtension());
+
+const warpProd = WarpFactory.forMainnet().use(new EthersExtension());
+
+const warp = import.meta.env.MODE === "development" ? warpDev : warpProd;
 
 export const contract = warp.contract(import.meta.env.VITE_WARP_CONTRACT);
