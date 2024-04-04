2b87a62040b6ae326c5c81a6ac25e68e54fc33e7
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-04-04 15:55:03 +1000

    Always apply cacheoptions

--- src/utils/warp.config.ts
+++ src/utils/warp.config.ts
@@ -1,13 +1,9 @@
 import { WarpFactory } from "warp-contracts";
 import { EthersExtension } from "warp-contracts-plugin-ethers";
 
-const warpDev = WarpFactory.forMainnet({
+const warp = WarpFactory.forMainnet({
   inMemory: true,
   dbLocation: "./cache/warp",
 }).use(new EthersExtension());
-
-const warpProd = WarpFactory.forMainnet().use(new EthersExtension());
-
-const warp = import.meta.env.MODE === "development" ? warpDev : warpProd;
 
 export const contract = warp.contract(import.meta.env.VITE_WARP_CONTRACT);
